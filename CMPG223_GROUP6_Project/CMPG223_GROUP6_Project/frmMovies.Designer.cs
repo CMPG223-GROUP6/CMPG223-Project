c54d2a0794470ca35f919bc26806a8a2f03c927f
Tracy-LeeOberem <tracylee846@gmail.com>
2024-08-19 21:50:05 +0200

    Movies Form Added
    Movies form added without comments

---
+++ CMPG223_GROUP6_Project/CMPG223_GROUP6_Project/frmMovies.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace CMPG223_GROUP6_Project
+{
+    partial class frmMovies
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.textBoxMovieDescription = new System.Windows.Forms.TextBox();
+            this.textBoxMovieName = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(411, 350);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 19;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(291, 350);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 18;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Price:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Description";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Name:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(291, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(440, 200);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(163, 214);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrice.TabIndex = 13;
+            // 
+            // textBoxMovieDescription
+            // 
+            this.textBoxMovieDescription.Location = new System.Drawing.Point(163, 171);
+            this.textBoxMovieDescription.Name = "textBoxMovieDescription";
+            this.textBoxMovieDescription.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMovieDescription.TabIndex = 12;
+            // 
+            // textBoxMovieName
+            // 
+            this.textBoxMovieName.Location = new System.Drawing.Point(163, 124);
+            this.textBoxMovieName.Name = "textBoxMovieName";
+            this.textBoxMovieName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMovieName.TabIndex = 11;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(152, 350);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 10;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // frmMovies
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxMovieDescription);
+            this.Controls.Add(this.textBoxMovieName);
+            this.Controls.Add(this.buttonAdd);
+            this.Name = "frmMovies";
+            this.Text = "frmMovies";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.TextBox textBoxMovieDescription;
+        private System.Windows.Forms.TextBox textBoxMovieName;
+        private System.Windows.Forms.Button buttonAdd;
+    }
+}
---
+++ CMPG223_GROUP6_Project/CMPG223_GROUP6_Project/frmMovies.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CMPG223_GROUP6_Project
+{
+    public partial class frmMovies : Form
+    {
+        private const string ConnectionString = @"";
+
+        public frmMovies()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            // Validate input
+            if (string.IsNullOrWhiteSpace(textBoxMovieName.Text) ||
+                string.IsNullOrWhiteSpace(textBoxMovieDescription.Text) ||
+                !decimal.TryParse(textBoxPrice.Text, out decimal price))
+            {
+                MessageBox.Show("Please enter valid movie details.");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+
+                    // Add new movie
+                    using (SqlCommand command = new SqlCommand("InsertMovie", conn))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@Movie_Name", textBoxMovieName.Text);
+                        command.Parameters.AddWithValue("@Movie_Description", textBoxMovieDescription.Text);
+                        command.Parameters.AddWithValue("@Price", price);
+                        command.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show("A new movie was added!");
+
+                    // Show all movies
+                    using (SqlCommand command = new SqlCommand("Show_Movies", conn))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        // Display records in DataGridView
+                        DataTable dt = new DataTable();
+                        dt.Load(command.ExecuteReader());
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a movie from the list to update.");
+                return;
+            }
+
+            var selectedRow = dataGridView1.SelectedRows[0];
+            int movieID = Convert.ToInt32(selectedRow.Cells["Movie_ID"].Value);
+
+            if (string.IsNullOrWhiteSpace(textBoxMovieName.Text) ||
+                string.IsNullOrWhiteSpace(textBoxMovieDescription.Text) ||
+                !decimal.TryParse(textBoxPrice.Text, out decimal price))
+            {
+                MessageBox.Show("Please enter valid movie details.");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+
+                    using (SqlCommand command = new SqlCommand("UpdateMovie", conn))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@Movie_ID", movieID);
+                        command.Parameters.AddWithValue("@Movie_Name", textBoxMovieName.Text);
+                        command.Parameters.AddWithValue("@Movie_Description", textBoxMovieDescription.Text);
+                        command.Parameters.AddWithValue("@Price", price);
+                        command.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show("Movie updated successfully!");
+                }
+
+                ShowMovies(); // Refresh the DataGridView
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a movie from the list to delete.");
+                return;
+            }
+
+            var selectedRow = dataGridView1.SelectedRows[0];
+            int movieID = Convert.ToInt32(selectedRow.Cells["Movie_ID"].Value);
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+
+                    using (SqlCommand command = new SqlCommand("DeleteMovie", conn))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@Movie_ID", movieID);
+                        command.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show("Movie deleted successfully!");
+                }
+
+                ShowMovies(); // Refresh DataGridView
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void ShowMovies()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+
+                    using (SqlCommand command = new SqlCommand("Show_Movies", conn))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        DataTable dt = new DataTable();
+                        dt.Load(command.ExecuteReader());
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
