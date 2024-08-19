746b42350d5a92fc6cf25c68b09850b13ff8cda5
M-kellerman <Markram.Kellerman@gmail.com>
2024-08-19 21:42:06 +0200

    Timeslot form added
    Added a functional timeslot form without too much creativity

---
+++ CMPG223_GROUP6_Project/CMPG223_GROUP6_Project/frmTimeslot.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace CMPG223_GROUP6_Project
+{
+    partial class frmTimeSlot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnShowAdd = new System.Windows.Forms.Button();
+            this.txtTimeslotAdd = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTimeslotUpdate = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIdUpdate = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtIdDelete = new System.Windows.Forms.TextBox();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(61, 44);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(920, 535);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.btnShowAdd);
+            this.tabPage1.Controls.Add(this.txtTimeslotAdd);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(912, 509);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Add Timeslot";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(452, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(353, 159);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Add timeslot: (hh/mm/ss)";
+            // 
+            // btnShowAdd
+            // 
+            this.btnShowAdd.Location = new System.Drawing.Point(124, 194);
+            this.btnShowAdd.Name = "btnShowAdd";
+            this.btnShowAdd.Size = new System.Drawing.Size(100, 23);
+            this.btnShowAdd.TabIndex = 1;
+            this.btnShowAdd.Text = "Add";
+            this.btnShowAdd.UseVisualStyleBackColor = true;
+            this.btnShowAdd.Click += new System.EventHandler(this.btnShowAdd_Click);
+            // 
+            // txtTimeslotAdd
+            // 
+            this.txtTimeslotAdd.Location = new System.Drawing.Point(216, 129);
+            this.txtTimeslotAdd.Name = "txtTimeslotAdd";
+            this.txtTimeslotAdd.Size = new System.Drawing.Size(100, 20);
+            this.txtTimeslotAdd.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.txtTimeslotUpdate);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.txtIdUpdate);
+            this.tabPage2.Controls.Add(this.btnUpdate);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(912, 509);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Edit Timeslot";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Timeslot:";
+            // 
+            // txtTimeslotUpdate
+            // 
+            this.txtTimeslotUpdate.Location = new System.Drawing.Point(73, 165);
+            this.txtTimeslotUpdate.Name = "txtTimeslotUpdate";
+            this.txtTimeslotUpdate.Size = new System.Drawing.Size(100, 20);
+            this.txtTimeslotUpdate.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ID:";
+            // 
+            // txtIdUpdate
+            // 
+            this.txtIdUpdate.Location = new System.Drawing.Point(73, 97);
+            this.txtIdUpdate.Name = "txtIdUpdate";
+            this.txtIdUpdate.Size = new System.Drawing.Size(100, 20);
+            this.txtIdUpdate.TabIndex = 3;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(200, 97);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(466, 35);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(295, 191);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.btnDelete);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.txtIdDelete);
+            this.tabPage3.Controls.Add(this.dataGridView3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(912, 509);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Delete Timeslot";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(27, 182);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(177, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Select ID:";
+            // 
+            // txtIdDelete
+            // 
+            this.txtIdDelete.Location = new System.Drawing.Point(104, 102);
+            this.txtIdDelete.Name = "txtIdDelete";
+            this.txtIdDelete.Size = new System.Drawing.Size(100, 20);
+            this.txtIdDelete.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(538, 75);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView3.TabIndex = 0;
+            // 
+            // frmTimeSlot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 622);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "frmTimeSlot";
+            this.Text = "frmTimeSlot";
+            this.Load += new System.EventHandler(this.frmTimeSlot_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnShowAdd;
+        private System.Windows.Forms.TextBox txtTimeslotAdd;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTimeslotUpdate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIdUpdate;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtIdDelete;
+        private System.Windows.Forms.DataGridView dataGridView3;
+    }
+}
---
+++ CMPG223_GROUP6_Project/CMPG223_GROUP6_Project/frmTimeslot.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CMPG223_GROUP6_Project
+{
+    public partial class frmTimeSlot : Form
+    {
+        SqlConnection con = new SqlConnection(@"");
+        SqlCommand command;
+        SqlDataReader reader;
+        SqlDataAdapter adapter;
+        DataSet dataSet;
+        public frmTimeSlot()
+        {
+            InitializeComponent();
+        }
+
+        private void showAdd()
+        {
+            try
+            {
+                con.Open();
+                command = new SqlCommand("SHowTimeSlot", con);
+                command.CommandType = CommandType.StoredProcedure;
+
+                DataTable dt = new DataTable();
+                dt.Load(command.ExecuteReader());
+                dataGridView1.DataSource = dt;
+
+                con.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+
+        private void showDelete()
+        {
+            try
+            {
+                con.Open();
+                command = new SqlCommand("SHowTimeSlot", con);
+                command.CommandType = CommandType.StoredProcedure;
+
+                DataTable dt = new DataTable();
+                dt.Load(command.ExecuteReader());
+                dataGridView3.DataSource = dt;
+
+                con.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+
+        private void showUpdate()
+        {
+            try
+            {
+                con.Open();
+                command = new SqlCommand("SHowTimeSlot", con);
+                command.CommandType = CommandType.StoredProcedure;
+
+                DataTable dt = new DataTable();
+                dt.Load(command.ExecuteReader());
+                dataGridView2.DataSource = dt;
+
+                con.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnShowAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                command = new SqlCommand("Add_TimeSlot", con);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@A_Time", txtTimeslotAdd.Text);
+                command.ExecuteNonQuery();
+                MessageBox.Show("A new timeslot has been added");
+
+
+
+                con.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            showAdd();
+            showUpdate();
+            showDelete();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                command = new SqlCommand("update_TimeSlot", con);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Timeslot_ID", txtIdUpdate.Text);
+                command.Parameters.AddWithValue("@A_Time", txtTimeslotUpdate.Text);
+
+                adapter = new SqlDataAdapter(command);
+
+                command.ExecuteNonQuery();
+                command.Dispose();
+
+                MessageBox.Show("The timeslot has been updated!");
+
+
+                con.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            showAdd();
+            showUpdate();
+            showDelete();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                command = new SqlCommand("DeleteTimeSlot", con);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Timeslot_ID", txtIdDelete.Text);
+
+
+                command.ExecuteNonQuery();
+
+
+                MessageBox.Show("Timeslot deleted sucessfully!");
+
+
+                con.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            showAdd();
+            showUpdate();
+            showDelete();
+        }
+
+        private void frmTimeSlot_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
