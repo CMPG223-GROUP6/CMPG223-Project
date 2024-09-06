b6a19b0046011ec9be80e750b641e9a174cd9c77
nicole1drinkwater <nicnacsdrinkwater@gmail.com>
2024-09-06 17:39:06 +0200

    Added employee form function

--- CMPG223_GROUP6_Project/CMPG223_GROUP6_Project/frmEmployees.cs
+++ CMPG223_GROUP6_Project/CMPG223_GROUP6_Project/frmEmployees.cs
@@ -240,8 +240,75 @@ namespace MaintainEmployees
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            string name, surname, cellNum, username, password;
+            bool isError = false;
 
-        }
+            name = txtNameAdd.Text;
+            surname = txtSurnameAdd.Text;
+            cellNum = txtCellNumAdd.Text;
+            username = txtUsernameAdd.Text;
+            password = txtPasswordAdd.Text;
+
+            //Validates the input.
+            if (name == "")
+            {
+                MessageBox.Show("Enter a name.");
+                isError = true;
+            }
+            else if (surname == "")
+            {
+                MessageBox.Show("Enter a surname.");
+                isError = true;
+            }
+            else if (cellNum == "")
+            {
+                MessageBox.Show("Enter a cell number.");
+                isError = true;
+            }
+            else if (username == "")
+            {
+                MessageBox.Show("Enter a username.");
+                isError = true;
+            }
+            else if (password == "")
+            {
+                MessageBox.Show("Enter a password.");
+                isError = true;
+            }
+
+            if (isError == false)
+            {
+                conn.Open();
+
+                //SQL run stored procedure
+                comm = new SqlCommand("Add_Employee", conn);
+                comm.CommandType = CommandType.StoredProcedure;
+                comm.Parameters.AddWithValue("@Is_Admin", chkIsAdminAdd.Checked);
+                comm.Parameters.AddWithValue("@Is_Active", chkActiveAdd.Checked);
+                comm.Parameters.AddWithValue("@Employee_Name", name);
+                comm.Parameters.AddWithValue("@Employee_Surname", surname);
+                comm.Parameters.AddWithValue("@Cellphone_Num", cellNum);
+                comm.Parameters.AddWithValue("@Username", username);
+                comm.Parameters.AddWithValue("@Password", password);
+                comm.ExecuteNonQuery();
+
+                MessageBox.Show("A new employee was added!");
+
+                conn.Close();
+                showEmployees();
+                populateComboBox();
+
+                //Clears input
+                txtNameAdd.Text = "";
+                txtSurnameAdd.Text = "";
+                txtUsernameAdd.Text = "";
+                txtPasswordAdd.Text = "";
+                txtCellNumAdd.Text = "";
+                chkActiveAdd.Checked = false;
+                chkIsAdminAdd.Checked = false;
+
+                txtNameAdd.Focus();
+            }
 
         private void btnDashboard_Click(object sender, EventArgs e)
         {
