cf9590df9e0a10bf36e0a07f706e752fcac9e78b
Tracy-LeeOberem <tracylee846@gmail.com>
2024-09-07 16:56:19 +0200

    Password Format
    Made passwords in star values

--- CMPG223_GROUP6_Project/CMPG223_GROUP6_Project/frmLogin.Designer.cs
+++ CMPG223_GROUP6_Project/CMPG223_GROUP6_Project/frmLogin.Designer.cs
@@ -75,6 +75,7 @@
             this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtPassword.Location = new System.Drawing.Point(8, 144);
             this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(152, 26);
             this.txtPassword.TabIndex = 4;
             this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
